b98e09fedfbbc808743e5c1ad4536f64452d3536
SoiChienLang <38004488+SoiChienLang@users.noreply.github.com>
2020-12-06 23:37:44 +0700

    REUPDATE

---
+++ adaptive_huffman/main.py
@@ -0,0 +1,10 @@
+﻿from Adaptive_huffman import compress,extract
+def main():
+    filein = 'adaptive_huffman\Lena.raw'
+    filecompress = 'compressed'
+    fileextract = 'extracted.raw'
+    compress(filein, filecompress)
+    extract(filecompress, fileextract)
+    input("Press ENTER to exit...")
+if __name__ == '__main__':
+    main()
